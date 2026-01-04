99c9c86a88597ef915f22d52b70c119e0d9f95e9
Yash Kulat <yashkulat1234@gmail.com>
2026-01-04 19:16:53 +0530

    new

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -4,10 +4,10 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './App.css'
 import Footer from './Files/Footer/Footer.jsx'
 import Header from './Files/Header/Header.jsx'
-import AboutPage from './Files/AboutPage/AboutPage.jsx'
-import ContactPage from './Files/Contact/ContactPage.jsx'
-import TermsConditions from './Files/Terms&Conditions/TermsConditions.jsx'
-import TermsPolicy from './Files/Terms&Policy/TermsPolicy.jsx'
+// import AboutPage from './Files/AboutPage/AboutPage.jsx'
+// import ContactPage from './Files/Contact/ContactPage.jsx'
+// import TermsConditions from './Files/Terms&Conditions/TermsConditions.jsx'
+// import TermsPolicy from './Files/Terms&Policy/TermsPolicy.jsx'
 // import SignIn from './Files/SignIn/SignIn.jsx'
 // import SignUp from './Files/SignUp/SignUp.jsx'
 import Home from './Files/MainPage/Home/Home.jsx'
@@ -30,14 +30,8 @@ function App() {
     <>
       {/* <Header/> */}
       {/* <Outlet/> */}
-      {/* <AboutPage/> */}
-      {/* <ContactPage/> */}
-      <TermsConditions/>
-      {/* <Footer/> */}
-      {/* <TermsPolicy/> */}
-      {/* <SignIn/> */}
-      {/* <SignUp /> */}
-      {/* <Home/> */}
+    
+      <Home/>
       {/* <ShopNow/> */}
       {/* <Fashion/> */}
       {/* <Electronics /> */}
